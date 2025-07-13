079a55a501fab33eb160e2f16774838a237ca188
Ranjuna120 <IT22236296@my.sliit.lk>
2025-07-13 19:57:33 +0530

    Update Register.js

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -31,6 +31,7 @@ const Register = () => {
 
     try {
       const response = await axios.post('http://localhost:8081/api/auth/register', {
+        name: formData.name,
         email: formData.email,
         password: formData.password
       });
