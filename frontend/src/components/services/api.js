71280a3df4903abce1252c9f21c4e11814440697
Ranjuna120 <IT22236296@my.sliit.lk>
2025-07-17 02:10:22 +0530

    Update api.js

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8082/api';
+const API_URL = 'http://localhost:8081/api';
 
 // Create axios instance with base configuration
 const api = axios.create({
