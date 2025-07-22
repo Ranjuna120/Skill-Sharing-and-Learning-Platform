e81342e1b633e1393f9f8442150c02197b3d8808
Ranjuna120 <IT22236296@my.sliit.lk>
2025-07-22 13:21:10 +0530

    Update Navigation.js

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import { FaHome, FaPlus, FaUser, FaBell, FaBook, FaSignOutAlt, FaGraduationCap } from 'react-icons/fa';
 import axios from 'axios';
 import './Navigation.css';
 
@@ -10,7 +11,7 @@ const Navigation = () => {
 
     const handleLogout = async () => {
         try {
-            await axios.post('http://localhost:8082/api/users/logout', {}, {
+            await axios.post('http://localhost:8081/api/users/logout', {}, {
                 withCredentials: true
             });
             logout();
@@ -21,23 +22,50 @@ const Navigation = () => {
     };
 
     return (
-        <nav className="navigation">
-            <div className="nav-brand">
-                <Link to="/">Skill Sharing</Link>
-            </div>
-            <div className="nav-links">
-                {user ? (
-                    <>
-                        <Link to="/posts">Posts</Link>
-                        <Link to="/create-post">Create Post</Link>
-                        <Link to={`/profile/${user.sub}`}>Profile</Link>
-                        <button onClick={handleLogout} className="logout-button">
-                            Logout
-                        </button>
-                    </>
-                ) : (
-                    <Link to="/login">Login</Link>
-                )}
+        <nav className="modern-navigation">
+            <div className="nav-container">
+                <div className="nav-brand">
+                    <Link to="/" className="brand-link">
+                        <FaGraduationCap className="brand-icon" />
+                        <span className="brand-text">Skill Sharing</span>
+                    </Link>
+                </div>
+                
+                <div className="nav-links">
+                    {user ? (
+                        <>
+                            <Link to="/posts" className="nav-link" title="Posts">
+                                <FaHome className="nav-icon" />
+                                <span className="nav-text">Home</span>
+                            </Link>
+                            <Link to="/create-post" className="nav-link" title="Create Post">
+                                <FaPlus className="nav-icon" />
+                                <span className="nav-text">Create</span>
+                            </Link>
+                            <Link to="/plans" className="nav-link" title="Learning Plans">
+                                <FaBook className="nav-icon" />
+                                <span className="nav-text">Plans</span>
+                            </Link>
+                            <Link to="/notifications" className="nav-link" title="Notifications">
+                                <FaBell className="nav-icon" />
+                                <span className="nav-text">Notifications</span>
+                            </Link>
+                            <Link to={`/profile/${user.sub || user.id}`} className="nav-link profile-link" title="Profile">
+                                <FaUser className="nav-icon" />
+                                <span className="nav-text">Profile</span>
+                            </Link>
+                            <button onClick={handleLogout} className="logout-button" title="Logout">
+                                <FaSignOutAlt className="nav-icon" />
+                                <span className="nav-text">Logout</span>
+                            </button>
+                        </>
+                    ) : (
+                        <Link to="/login" className="nav-link login-link">
+                            <FaUser className="nav-icon" />
+                            <span className="nav-text">Login</span>
+                        </Link>
+                    )}
+                </div>
             </div>
         </nav>
     );
